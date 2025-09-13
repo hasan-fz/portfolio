6acbc6999cd8708347718130c8466f24c5a76f03
Hasan Faraz <hfaraz1123@gmail.com>
2025-09-13 11:40:10 +0530

    Initial commit with project files

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// newer flat config format
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier'; // 👈 import it
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+]);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return(
+  <div className='min-h-screen'>
+    <div className="text-center">
+        Full Stack Developer.<br/>
+        Building elegant solutions.
+    </div>
+  </div>
+  );
+};
